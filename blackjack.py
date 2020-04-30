dd3ca2113dc35267b41a53a604dade17b1c7a4e9
cabukberat <cabukberat@gmail.com>
2020-04-30 19:04:52 +0300

    added comments and a docstring for calc_hand

--- blackjack.py
+++ blackjack.py
@@ -6,9 +6,10 @@ import os
 
 
 def calc_hand(hand):
-
+    """Function used to calculate hand value. Takes a list as an input, returns an integer."""
     handval = 0
-    
+    special_cards = ["J", "K", "Q", "A"]
+
     for i in hand:
         if i not in special_cards:
             handval += int(i)
@@ -21,10 +22,9 @@ def calc_hand(hand):
                 handval += 1
 
     return handval              
+              
 
-
-special_cards = ["J", "K", "Q", "A"]               
-
+#the deck
 cards=[ '2','3','4','5','6','7','8','9','10','J','K','Q','A',
           '2','3','4','5','6','7','8','9','10','J','K','Q','A',
           '2','3','4','5','6','7','8','9','10','J','K','Q','A',
@@ -32,30 +32,37 @@ cards=[ '2','3','4','5','6','7','8','9','10','J','K','Q','A',
 ]
 
 
+#shuffle the deck
 random.shuffle(cards)
 
+#initialize the hands
 dealer = []
 player = []
 
+#draw some cards
 player.append(cards.pop())
 dealer.append(cards.pop())
 player.append(cards.pop())
 dealer.append(cards.pop())
 
+#initialize the turn counter
 turn_count = 0
 
+#main loop
 while turn_count >= 0:
 
-    player_points = calc_hand(player)
-    dealer_points = calc_hand(dealer)
+    player_points = calc_hand(player)   #calculate player's hand
+    dealer_points = calc_hand(dealer)   #calculate dealer's hand
 
     print(f"player's cards: {player}, points: {player_points}")
 
+    #dealer's second card is hidden for the first turn
     if turn_count == 0:
-        print(f"dealer's cards: {dealer[0]} '?', points: {dealer_points}")
+        print(f"dealer's cards: {dealer[0]} '?', points: {dealer_points}") 
     else:
         print(f"dealer's cards: {dealer}, points: {dealer_points}")
 
+    #bunch of win conditions
     if player_points == 21:
         print('you win')
         answer1 = 0
@@ -71,6 +78,7 @@ while turn_count >= 0:
     else:
 	    answer1 = input('devam etmek ister misiniz?\n>')
 
+    #oyuncu pas geçerse
     if answer1 == 'hayır':
     
         while True:
@@ -90,7 +98,8 @@ while turn_count >= 0:
                 break
             else:
                 continue
-                
+
+    #oyuncu kart çekerse           
     if answer1 == 'evet':
         turn_count += 1
         player.append(cards.pop())
